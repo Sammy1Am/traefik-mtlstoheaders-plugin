3d680eff85fcd93ca4e4ac5c811eed2fc6ebc537
Sammy1Am <467704+Sammy1Am@users.noreply.github.com>
2025-10-12 19:29:55 +0000

    Success on rewrite from template!

--- mtls_to_headers.go
+++ mtls_to_headers.go
@@ -1,23 +1,18 @@
-package mtlstoheaders
+package traefik_mtlstoheaders_plugin
 
 import (
 	"context"
 	"crypto/x509"
-	"crypto/x509/pkix"
 	"encoding/pem"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
-
-	"log"
 )
 
-const typeName = "PassClientTLSCert"
-
 const (
-	xForwardedTLS           = "X-Forwarded-Tls-"
-	xForwardedTLSClientCert = "X-Forwarded-Tls-Client-Cert"
+	xForwardedTLS = "X-Forwarded-Tls-"
 )
 
 const (
@@ -25,128 +20,70 @@ const (
 	subFieldSeparator = ","
 )
 
-var attributeTypeNames = map[string]string{
-	"0.9.2342.19200300.100.1.25": "DC", // Domain component OID - RFC 2247
+// Config the plugin configuration.
+type Config struct {
+	PEM  string                    `json:"pem,omitempty"`
+	Info *tlsClientCertificateInfo `json:"info,omitempty"`
 }
 
-// IssuerDistinguishedNameOptions is a struct for specifying the configuration
-// for the distinguished name info of the issuer. This information is defined in
-// RFC3739, section 3.1.1.
-type IssuerDistinguishedNameOptions struct {
-	CommonName          string
-	CountryName         string
-	DomainComponent     string
-	LocalityName        string
-	OrganizationName    string
-	SerialNumber        string
-	StateOrProvinceName string
-}
-
-func newIssuerDistinguishedNameOptions(info *IssuerDistinguishedNameOptions) *IssuerDistinguishedNameOptions {
-	if info == nil {
-		return nil
-	}
-
-	return &IssuerDistinguishedNameOptions{
-		CommonName:          info.CommonName,
-		CountryName:         info.CountryName,
-		DomainComponent:     info.DomainComponent,
-		LocalityName:        info.LocalityName,
-		OrganizationName:    info.OrganizationName,
-		SerialNumber:        info.SerialNumber,
-		StateOrProvinceName: info.StateOrProvinceName,
-	}
-}
-
-// SubjectDistinguishedNameOptions is a struct for specifying the configuration
-// for the distinguished name info of the subject. This information is defined
-// in RFC3739, section 3.1.2.
-type SubjectDistinguishedNameOptions struct {
-	CommonName             string
-	CountryName            string
-	DomainComponent        string
-	LocalityName           string
-	OrganizationName       string
-	OrganizationalUnitName string
-	SerialNumber           string
-	StateOrProvinceName    string
-}
-
-func newSubjectDistinguishedNameOptions(info *SubjectDistinguishedNameOptions) *SubjectDistinguishedNameOptions {
-	if info == nil {
-		return nil
-	}
-
-	return &SubjectDistinguishedNameOptions{
-		CommonName:             info.CommonName,
-		CountryName:            info.CountryName,
-		DomainComponent:        info.DomainComponent,
-		LocalityName:           info.LocalityName,
-		OrganizationName:       info.OrganizationName,
-		OrganizationalUnitName: info.OrganizationalUnitName,
-		SerialNumber:           info.SerialNumber,
-		StateOrProvinceName:    info.StateOrProvinceName,
-	}
-}
-
-// tlsClientCertificateInfo is a struct for specifying the configuration for the passTLSClientCert middleware.
 type tlsClientCertificateInfo struct {
 	notAfter     string
 	notBefore    string
-	sans         *tlsClientCertificateSans
-	subject      *SubjectDistinguishedNameOptions
-	issuer       *IssuerDistinguishedNameOptions
 	serialNumber string
+	SANS         *tlsClientCertificateSans `json:"sans,omitempty"`
 }
 
 type tlsClientCertificateSans struct {
-	dns   string
-	email string
-	ip    string
-	uri   string
+	DNS   string `json:"dns,omitempty"`
+	Email string `json:"email,omitempty"`
+	Ip    string `json:"ip,omitempty"`
+	URI   string `json:"uri,omitempty"`
 }
 
-func newTLSClientCertificateInfo(info *tlsClientCertificateInfo) *tlsClientCertificateInfo {
-	if info == nil {
-		return nil
-	}
-
-	return &tlsClientCertificateInfo{
-		issuer:       newIssuerDistinguishedNameOptions(info.issuer),
-		notAfter:     info.notAfter,
-		notBefore:    info.notBefore,
-		subject:      newSubjectDistinguishedNameOptions(info.subject),
-		serialNumber: info.serialNumber,
-		sans:         info.sans,
-	}
+// CreateConfig creates the default plugin configuration.
+func CreateConfig() *Config {
+	return &Config{}
 }
 
-// mtlsToHeaders is a middleware that parses mTLS certificates to pass to other services.
-type mtlsToHeaders struct {
+// Demo a Demo plugin.
+type MTLSToHeaders struct {
 	next http.Handler
 	name string
-	pem  string                    // pass the sanitized pem to the backend in a specific header
-	info *tlsClientCertificateInfo // pass selected information from the client certificate
+	pem  string
+	info *tlsClientCertificateInfo
 }
 
-// New constructs a new mTLSToHeaders instance from supplied frontend header struct.
-func New(ctx context.Context, next http.Handler, config mtlsToHeaders, name string) (http.Handler, error) {
-	log.Print("Creating Middleware")
+// New created a new Demo plugin.
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	if len(config.PEM) == 0 {
+		return nil, fmt.Errorf("headers cannot be empty")
+	}
 
-	return &mtlsToHeaders{
+	return &MTLSToHeaders{
+		pem:  config.PEM,
+		info: config.Info,
 		next: next,
 		name: name,
-		pem:  config.pem,
-		info: newTLSClientCertificateInfo(config.info),
 	}, nil
 }
 
-func (p *mtlsToHeaders) GetTracingInformation() (string, string) {
-	return p.name, typeName
-}
+func (p *MTLSToHeaders) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	// for key, value := range a.headers {
+	// 	tmpl, err := a.template.Parse(value)
+	// 	if err != nil {
+	// 		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	// 		return
+	// 	}
 
-func (p *mtlsToHeaders) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	// 	writer := &bytes.Buffer{}
 
+	// 	err = tmpl.Execute(writer, req)
+	// 	if err != nil {
+	// 		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	// 		return
+	// 	}
+
+	// }
 	if p.pem != "" {
 		if req.TLS != nil && len(req.TLS.PeerCertificates) > 0 {
 			req.Header.Set(xForwardedTLS+p.pem, strings.TrimSuffix(getCertificates(req.TLS.PeerCertificates), subFieldSeparator))
@@ -154,7 +91,6 @@ func (p *mtlsToHeaders) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 			log.Print("Tried to extract a certificate on a request without mutual TLS")
 		}
 	}
-
 	if p.info != nil {
 		if req.TLS != nil && len(req.TLS.PeerCertificates) > 0 {
 			p.extractCertInfo(req.TLS.PeerCertificates, req)
@@ -163,124 +99,9 @@ func (p *mtlsToHeaders) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		}
 	}
 
+	req.Header.Set("Test", "Success!")
+
 	p.next.ServeHTTP(rw, req)
-}
-
-// extractCertInfo Writes cert info to headers
-// - the `,` is used to separate values in a single field
-// - if a field is empty, the field is ignored.
-func (p *mtlsToHeaders) extractCertInfo(certs []*x509.Certificate, req *http.Request) {
-
-	for _, peerCert := range certs {
-
-		if p.info != nil {
-			extractSubjectDNInfo(p.info.subject, &peerCert.Subject, req)
-			extractIssuerDNInfo(p.info.issuer, &peerCert.Subject, req)
-
-			if p.info.serialNumber != "" && peerCert.SerialNumber != nil {
-				sn := peerCert.SerialNumber.String()
-				if sn != "" {
-					writeHeaderValue(req, p.info.serialNumber, sn)
-				}
-			}
-
-			if p.info.notBefore != "" {
-				writeHeaderValue(req, p.info.notBefore, fmt.Sprintf("%d", uint64(peerCert.NotBefore.Unix())))
-			}
-
-			if p.info.notAfter != "" {
-				writeHeaderValue(req, p.info.notAfter, fmt.Sprintf("%d", uint64(peerCert.NotAfter.Unix())))
-			}
-
-			extractSANs(p.info.sans, peerCert, req)
-		}
-	}
-}
-
-func extractIssuerDNInfo(options *IssuerDistinguishedNameOptions, cs *pkix.Name, req *http.Request) {
-	if options == nil {
-		return
-	}
-
-	content := &strings.Builder{}
-
-	// Manage non-standard attributes
-	for _, name := range cs.Names {
-		// Domain Component - RFC 2247
-		if options.DomainComponent != "" && attributeTypeNames[name.Type.String()] == "DC" {
-			_, _ = fmt.Fprintf(content, "DC=%s%s", name.Value, subFieldSeparator)
-		}
-	}
-
-	if options.CountryName != "" {
-		writeHeaderValues(req, options.CountryName, cs.Country)
-	}
-
-	if options.StateOrProvinceName != "" {
-		writeHeaderValues(req, options.StateOrProvinceName, cs.Province)
-	}
-
-	if options.LocalityName != "" {
-		writeHeaderValues(req, options.LocalityName, cs.Locality)
-	}
-
-	if options.OrganizationName != "" {
-		writeHeaderValues(req, options.OrganizationName, cs.Organization)
-	}
-
-	if options.SerialNumber != "" {
-		writeHeaderValue(req, options.SerialNumber, cs.SerialNumber)
-	}
-
-	if options.CommonName != "" {
-		writeHeaderValue(req, options.CommonName, cs.CommonName)
-	}
-}
-
-func extractSubjectDNInfo(options *SubjectDistinguishedNameOptions, cs *pkix.Name, req *http.Request) string {
-	if options == nil {
-		return ""
-	}
-
-	content := &strings.Builder{}
-
-	// Manage non standard attributes
-	for _, name := range cs.Names {
-		// Domain Component - RFC 2247
-		if options.DomainComponent != "" && attributeTypeNames[name.Type.String()] == "DC" {
-			_, _ = fmt.Fprintf(content, "DC=%s%s", name.Value, subFieldSeparator)
-		}
-	}
-
-	if options.CountryName != "" {
-		writeHeaderValues(req, options.CountryName, cs.Country)
-	}
-
-	if options.StateOrProvinceName != "" {
-		writeHeaderValues(req, options.StateOrProvinceName, cs.Province)
-	}
-
-	if options.LocalityName != "" {
-		writeHeaderValues(req, options.LocalityName, cs.Locality)
-	}
-
-	if options.OrganizationName != "" {
-		writeHeaderValues(req, options.OrganizationName, cs.Organization)
-	}
-
-	if options.OrganizationalUnitName != "" {
-		writeHeaderValues(req, options.OrganizationalUnitName, cs.OrganizationalUnit)
-	}
-
-	if options.SerialNumber != "" {
-		writeHeaderValue(req, options.SerialNumber, cs.SerialNumber)
-	}
-
-	if options.CommonName != "" {
-		writeHeaderValue(req, options.CommonName, cs.CommonName)
-	}
-
-	return content.String()
 }
 
 func writeHeaderValue(req *http.Request, headerSuffix string, value string) {
@@ -288,16 +109,9 @@ func writeHeaderValue(req *http.Request, headerSuffix string, value string) {
 }
 
 func writeHeaderValues(req *http.Request, headerSuffix string, values []string) {
+	log.Printf("Invalues")
 	req.Header.Set(xForwardedTLS+headerSuffix, url.QueryEscape(strings.Join(values, subFieldSeparator)))
-}
-
-// sanitize As we pass the raw certificates, remove the useless data and make it http request compliant.
-func sanitize(cert []byte) string {
-	return strings.NewReplacer(
-		"-----BEGIN CERTIFICATE-----", "",
-		"-----END CERTIFICATE-----", "",
-		"\n", "",
-	).Replace(string(cert))
+	log.Printf("Done values")
 }
 
 // getCertificates Build a string with the client certificates.
@@ -322,41 +136,70 @@ func extractCertificate(cert *x509.Certificate) string {
 	return sanitize(certPEM)
 }
 
+// sanitize As we pass the raw certificates, remove the useless data and make it http request compliant.
+func sanitize(cert []byte) string {
+	return strings.NewReplacer(
+		"-----BEGIN CERTIFICATE-----", "",
+		"-----END CERTIFICATE-----", "",
+		"\n", "",
+	).Replace(string(cert))
+}
+
+// extractCertInfo Writes cert info to headers
+// - the `,` is used to separate values in a single field
+// - if a field is empty, the field is ignored.
+func (p *MTLSToHeaders) extractCertInfo(certs []*x509.Certificate, req *http.Request) {
+	for _, peerCert := range certs {
+		if p.info.serialNumber != "" && peerCert.SerialNumber != nil {
+			sn := peerCert.SerialNumber.String()
+			if sn != "" {
+				writeHeaderValue(req, p.info.serialNumber, sn)
+			}
+		}
+
+		if p.info.notBefore != "" {
+			writeHeaderValue(req, p.info.notBefore, fmt.Sprintf("%d", uint64(peerCert.NotBefore.Unix())))
+		}
+
+		if p.info.notAfter != "" {
+			writeHeaderValue(req, p.info.notAfter, fmt.Sprintf("%d", uint64(peerCert.NotAfter.Unix())))
+		}
+
+		if p.info != nil {
+			extractSANs(p.info.SANS, peerCert, req)
+		}
+
+	}
+}
+
 // getSANs get the Subject Alternate Name values.
 func extractSANs(options *tlsClientCertificateSans, cert *x509.Certificate, req *http.Request) {
 	if cert == nil {
 		return
 	}
-
-	if options.dns != "" {
-		writeHeaderValues(req, options.dns, cert.DNSNames)
+	log.Printf("InSANS")
+	if options.DNS != "" {
+		writeHeaderValues(req, options.DNS, cert.DNSNames)
 	}
 
-	if options.email != "" {
-		writeHeaderValues(req, options.dns, cert.EmailAddresses)
+	if options.Email != "" {
+		log.Printf("InEmail")
+		writeHeaderValues(req, options.Email, cert.EmailAddresses)
 	}
 
-	if options.ip != "" {
-		writeHeaderValues(req, options.dns, cert.DNSNames)
-	}
-
-	if options.uri != "" {
-		writeHeaderValues(req, options.dns, cert.DNSNames)
-	}
-
-	if options.ip != "" {
+	if options.Ip != "" {
 		var ips []string
 		for _, ip := range cert.IPAddresses {
 			ips = append(ips, ip.String())
 		}
-		writeHeaderValues(req, options.ip, ips)
+		writeHeaderValues(req, options.Ip, ips)
 	}
 
-	if options.uri != "" {
+	if options.URI != "" {
 		var uris []string
 		for _, uri := range cert.URIs {
 			uris = append(uris, uri.String())
 		}
-		writeHeaderValues(req, options.uri, uris)
+		writeHeaderValues(req, options.URI, uris)
 	}
 }
